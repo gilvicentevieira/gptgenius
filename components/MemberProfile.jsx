106a9822477d3e3eb101d7cf711bc94bfe84f5b4
Gil <gilvicentevieira@gmail.com>
2024-01-22 00:59:36 +0000

    Added sidebar components

---
+++ components/MemberProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MemberProfile = () => {
+  return (
+    <div>
+      MemberProfile
+    </div>
+  )
+}
+
+export default MemberProfile
+MemberProfile
---
+++ components/SideBarHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBarHeader = () => {
+  return (
+    <div>
+      SideBarHeader
+    </div>
+  )
+}
+
+export default SideBarHeader
