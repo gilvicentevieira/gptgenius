8dac6b730bf09e10d661766ff36f83dc79d85281
Gil <gilvicentevieira@gmail.com>
2024-01-22 01:09:23 +0000

    NavLinks component impl

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import Link from "next/link"
+
+const links = [
+    {href:'/chat', label:'chat'},
+    {href:'/tours', label:'tours'},
+    {href:'/tours/new-tour', label:'new tour'},
+    {href:'/profile', label:'profile'}
+]
 
 const NavLinks = () => {
   return (
-    <div>
-      NavLinks
-    </div>
+    <ul className="menu text-base-content">
+      {links.map((link)=>(
+        <li key={link.href}>
+            <Link href={link.href} className="capitalize">
+                {link.label}
+            </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
 
