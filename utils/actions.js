2add1ca34f2d387570bfedb6e18cde8e05b961d8
Gil <gilvicentevieira@gmail.com>
2024-01-22 21:42:03 +0000

    Setting up logic for the array with the chat context

--- utils/actions.js
+++ utils/actions.js
@@ -5,18 +5,23 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 })
 
-export const generateChatResponse = async(chatMessage)=>{
-    const response = await openai.chat.completions.create({
-        messages: [
-            {role: 'system', content:'You are a helpful assistant'},
-            {role: 'user', content: chatMessage}
-        ],
-        model: 'gpt-3.5-turbo',
-        temperature: 0
-    })
+export const generateChatResponse = async(chatMessages)=>{
 
-    console.log(chatMessage)
-    console.log(response.choices[0].message)
-    console.log(response)
-    return 'awesome'
+    try{
+        const response = await openai.chat.completions.create({
+            messages: [
+                {role: 'system', content:'You are a helpful assistant'},
+                ...chatMessages
+            ],
+            model: 'gpt-3.5-turbo',
+            temperature: 0
+        })
+    
+        return response.choices[0].message
+    }catch(err){
+        console.log(err)
+        return null
+    }
+
+
 }
