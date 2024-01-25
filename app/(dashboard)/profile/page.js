aa92e5bf724ab297ba0b88cff3839eff0dddf4dc
Gil <gilvicentevieira@gmail.com>
2024-01-25 20:49:13 +0000

    Added tokens value to profile page

--- app/(dashboard)/profile/page.js
+++ app/(dashboard)/profile/page.js
@@ -1,10 +1,20 @@
-import { UserProfile } from '@clerk/nextjs'
+import { fetchUserTokensById } from '@/utils/actions'
+import { UserProfile, auth } from '@clerk/nextjs'
 import React from 'react'
 
-const ProfilePage = () => {
-  return (
-    <UserProfile/>
-  )
+const ProfilePage = async() => {
+
+  const {userId} = auth()
+  const currentTokens = await fetchUserTokensById(userId)
+
+    return (
+      <div>
+        <h2 className='mb-8 ml-8 text-xl font-extrabold'>
+          Token amount: {currentTokens}
+        </h2>
+        <UserProfile/>
+      </div>
+    )
 }
 
 export default ProfilePage
