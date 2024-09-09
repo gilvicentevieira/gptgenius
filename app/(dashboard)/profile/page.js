b0710b0d8381a6ac418a5f1529812d145fda8804
Gil <gilvicentevieira@gmail.com>
2024-09-10 00:49:00 +0100

    removing tokens logic from profile page

--- app/(dashboard)/profile/page.js
+++ app/(dashboard)/profile/page.js
@@ -1,17 +1,12 @@
-import { fetchUserTokensById } from '@/utils/actions'
 import { UserProfile, auth } from '@clerk/nextjs'
 import React from 'react'
 
 const ProfilePage = async() => {
 
   const {userId} = auth()
-  const currentTokens = await fetchUserTokensById(userId)
 
     return (
       <div>
-        <h2 className='mb-8 ml-8 text-xl font-extrabold'>
-          Token amount: {currentTokens}
-        </h2>
         <UserProfile/>
       </div>
     )
