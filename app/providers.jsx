c121b07806bbc3fc8d1597ac7351e78e73b0d178
Gil <gilvicentevieira@gmail.com>
2024-09-10 00:49:40 +0100

    providers comp code format

--- app/providers.jsx
+++ app/providers.jsx
@@ -5,23 +5,23 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { useState } from "react"
 import { Toaster } from "react-hot-toast"
 
-const Providers = ({children}) => {
-    const [queryClient] = useState(()=>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 60 * 1000,
-          }
+const Providers = ({ children }) => {
+  const [queryClient] = useState(() =>
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 60 * 1000,
         }
-      })
-    )
-    return (
-      <QueryClientProvider client={queryClient}>
-        <Toaster position="top-center"/>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false}/>
-      </QueryClientProvider>
-    )
+      }
+    })
+  )
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Toaster position="top-center" />
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default Providers
