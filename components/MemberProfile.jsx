ce3854c4cdd343bc8bb1da62dd4b20779c76f5cb
Gil <gilvicentevieira@gmail.com>
2024-09-10 00:48:19 +0100

    removing tokens amount from memberprofile comp

--- components/MemberProfile.jsx
+++ components/MemberProfile.jsx
@@ -1,15 +1,8 @@
-import { fetchUserTokensById, generateUserTokensForId } from "@/utils/actions"
 import { UserButton, currentUser, auth } from "@clerk/nextjs"
 
 const MemberProfile = async () => {
     const user = await currentUser()
     const {userId} = auth()
-
-    let tokens
-    tokens = await fetchUserTokensById(userId)
-    if(!tokens){
-        tokens = await generateUserTokensForId(userId)
-    }
 
     return (
         <div className="px-4 flex items-center gap-2">
